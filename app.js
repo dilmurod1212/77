6ab335bf54e878cd1e027c2c658ca457fd0ca894
dilmurod <dilmurod121202@gmail.com>
2023-09-10 16:49:07 +0500

    search panel

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ const category = [
     image: "./images/category-icons/Component1(20).svg",
   },
 ];
-const Cards = [
+let Cards = [
   {
     image: "./images/card-img/Image(1).png",
     address: "г. Самарканд",
@@ -218,32 +218,6 @@ category.forEach((item) => {
   `;
 });
 
-const cardItems = document.querySelector(".card-items");
-Cards.forEach((item) => {
-  cardItems.innerHTML += `
-  <div class="card bg-white rounded-lg shadow overflow-hidden cursor-pointer">
-  <img src="${item.image}" alt="" class="w-full object-cover" />
-  <div class="content p-4">
-    <span class="py-1 px-2 bg-gray-200 rounded-lg text-xs my-4">${item.address}</span>
-    <h5
-      class="my-2 text-xl font-bold tracking-tight text-[#388FF3] max-md:text-base"
-    >
-      ${item.title}
-    </h5>
-    <p class="my-2 opacity-50 text-sm">Вчера, 19:20</p>
-    <a
-      href="tel:+998 71 200 70 07"
-      class="text-lg font-bold opacity-60 max-md:text-sm"
-      >+998 71 200 70 07</a
-    >
-    <p class="text-black font-bold text-2xl max-md:text-xl">
-      ${item.price} <span class="text-[#388FF3] text-xs">UZS</span>
-    </p>
-  </div>
-</div>
-  `;
-});
-
 const card = document.querySelectorAll(".card");
 // import { uiRender, obj } from "./product.js";
 card.forEach((item) => {
@@ -294,3 +268,52 @@ function closeModal() {
 }
 
 // dropdown
+
+// search panel
+const products = [...new Set(Cards.map((item) => item))];
+
+const searchInput = document.querySelector(".search-input");
+searchInput.addEventListener("keyup", (e) => {
+  const value = e.target.value.toLowerCase();
+  const newArr = products.filter((item) =>
+    item.title.toLowerCase().includes(value)
+  );
+  if (newArr.length) {
+    cardRender(newArr);
+  } else {
+    const card = document.querySelector(".card-items");
+    card.innerHTML = `<h1 class="text-2xl text-blue-400 w-[200%] font-bold">Bunday mahsulot mavjud emas😢</h1>`;
+  }
+});
+
+function cardRender(arr) {
+  const card = document.querySelector(".card-items");
+  card.innerHTML = arr
+    .map((item) => {
+      return `
+    <div class="card bg-white rounded-lg shadow overflow-hidden cursor-pointer">
+    <img src="${item.image}" alt="" class="w-full object-cover" />
+    <div class="content p-4">
+      <span class="py-1 px-2 bg-gray-200 rounded-lg text-xs my-4">${item.address}</span>
+      <h5
+        class="my-2 text-xl font-bold tracking-tight text-[#388FF3] max-md:text-base"
+      >
+        ${item.title}
+      </h5>
+      <p class="my-2 opacity-50 text-sm">Вчера, 19:20</p>
+      <a
+        href="tel:+998 71 200 70 07"
+        class="text-lg font-bold opacity-60 max-md:text-sm"
+        >+998 71 200 70 07</a
+      >
+      <p class="text-black font-bold text-2xl max-md:text-xl">
+        ${item.price} <span class="text-[#388FF3] text-xs">UZS</span>
+      </p>
+    </div>
+  </div>
+    `;
+    })
+    .join("");
+}
+cardRender(products);
+
